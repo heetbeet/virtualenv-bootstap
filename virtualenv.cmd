185a57846e8060d816246bacfb20a381c8d2cb3c
heetbeet <4103775+heetbeet@users.noreply.github.com>
2024-07-27 23:03:11 +0200

    Add --python dispatching mechanism to just run python.exe directly

--- virtualenv.cmd
+++ virtualenv.cmd
@@ -4,22 +4,26 @@
 
     set "_cmd=%~f0"
     set _arg1=%1
+    set _arg2=%2
     set _args=%*
 
     powershell -nologo -nop -exec bypass "iex (Get-Content '%_cmd%' -Raw)"
     goto :EOF
 #>
 
-# Retrieve the value from the environment variable
+# The first arg is allowed to be a x.y.z version number to dispatch a specefic version of Python
 $version = $env:_arg1 -replace '"', ''
 $pattern = '^3\.\d+\.\d+$'
+
+# If the first or second argument is --python then the rest of the args is passed to python (not virtualenv)
+$runpython_arg1 = (($env:_arg1 -replace '"', '') -eq '--python')
+$runpython_arg2 = (($env:_arg2 -replace '"', '') -eq '--python')
 
 $url = "https://www.nuget.org/api/v2/package/python"
 if ($version -match $pattern) {
     $url = "https://www.nuget.org/api/v2/package/python/$version"
 }
 
-$basePath = Resolve-Path -Path "$env:_cmd\..\.." -Relative
 $fileName = $url.Split("/")[-1] + ".zip"
 $dirName = $fileName -replace ".zip$", ""
 $tempBase = Join-Path -Path $env:TEMP -ChildPath "virtualenv-python-bases/$dirName"
@@ -28,9 +32,8 @@ $zipPath = Join-Path -Path $tempBase -ChildPath $fileName
 $pydirPath = Join-Path -Path $tempBase -ChildPath "python"
 New-Item -Path $pydirPath -ItemType Directory -Force  | Out-Null
 
-$pydirPath = Resolve-Path -Path $pydirPath -Relative
-
 # Define the paths
+$pydirPath = Resolve-Path -Path $pydirPath -Relative
 $pythonExePath = Join-Path -Path $pydirPath -ChildPath "tools\python.exe"
 $virtualenvExePath = Join-Path -Path $pydirPath -ChildPath "tools\Scripts\virtualenv.exe"
 
@@ -41,6 +44,7 @@ $virtualenvExists = Test-Path -Path $virtualenvExePath
 if ($pythonExists -and $virtualenvExists ) {
     # no work needed
 } else {
+    Write-Output "Downloading '$($url)' and running 'pip install virtualenv'..."
 
     # Download the Python zip
     try {
@@ -54,19 +58,29 @@ if ($pythonExists -and $virtualenvExists ) {
         }
     }
 
-    # Extract the zip
+    # Extract python and pip install virtualenv
     Expand-Archive -Path $zipPath -DestinationPath $pydirPath -Force 
-
-    # Execute Python command to download and run get-pip.py
-    Write-Output "First time is slow, running `pip install virtualenv`..."
     & $pythonExePath -m pip install virtualenv -q --no-warn-script-location
 }
 
-
+# Skip the argstring all the way past the <version> section
 $argstring = $env:_args
 if ($version -match $pattern) {
     $argstring = $argstring.Substring(($env:_arg1).Length)
 }
 
-# Run virtualenv with the collected args
-& cmd /c "`"$pythonExePath`" -m virtualenv $argstring"
+# Run python else virtualenv with the collected args
+if ($runpython_arg1) {
+    # Skip the argstring all the way past the --python section
+    $argstring = ($argstring -replace '^\s+', '').Substring(($env:_arg1 -replace '^\s+', '').Length)
+    & cmd /c "`"$pythonExePath`" $argstring"
+
+} elseif ($runpython_arg2) {
+    # Skip the argstring all the way past the --python section
+    $argstring = ($argstring -replace '^\s+', '').Substring(($env:_arg2 -replace '^\s+', '').Length)
+    & cmd /c "`"$pythonExePath`" $argstring"
+
+} else  {
+    # Run virtualenv with the collected args
+    & cmd /c "`"$pythonExePath`" -m virtualenv $argstring"
+}
